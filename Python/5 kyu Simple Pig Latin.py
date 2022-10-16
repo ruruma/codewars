79d8154ac989b21f9964083b0ba10c0ab418db06
unknown <seymurguseynov77@gmail.com>
2022-10-16 13:58:22 +0400

    better solution for last question

--- Python/5 kyu Simple Pig Latin.py
+++ Python/5 kyu Simple Pig Latin.py
@@ -1,15 +1,21 @@
 import string
 
 
-def pig_it(text):
-    x = text.split()
-    res = []
-    punc = string.punctuation
+# def pig_it(text):
+#     x = text.split()
+#     res = []
+#     punc = string.punctuation
+#
+#     for count, item in enumerate(x):
+#         if item not in punc:
+#             item = (item + item[0] + 'ay')[1::]
+#             res.append(item)
+#         else:
+#             res.append(item)
+#     return " ".join(res)
 
-    for count, item in enumerate(x):
-        if item not in punc:
-            item = (item + item[0] + 'ay')[1::]
-            res.append(item)
-        else:
-            res.append(item)
-    return " ".join(res)
+
+# better solution
+# return ' '.join([word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in x])
+
+
